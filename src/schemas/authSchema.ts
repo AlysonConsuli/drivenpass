3cb2c5ab781bef4adfccf8600c245d2e9f2cf658
Mateus Brasil <Mateus-code-98>
2022-08-03 09:07:21 -0300

    Preparando revisão de código

--- src/schemas/authSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-import { UserInsertData } from "../services/authService.js";
-
-export const authSchema = Joi.object<UserInsertData>({
-  email: Joi.string().email().required(),
-  password: Joi.string().required().min(10),
-});
--- src/utils/categoryUtils.ts
+++
@@ -1,7 +0,0 @@
-import Cryptr from "cryptr";
-import "../config/setup.js";
-
-const cryptr = new Cryptr(process.env.CRYPTR_SECRET_KEY);
-
-export const encrypt = (string: string) => cryptr.encrypt(string);
-export const decrypt = (string: string) => cryptr.decrypt(string);
